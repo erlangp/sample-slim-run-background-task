bdd6aba9ac50605bea67e1b326dd7425cdade0d8
Erlang Parasu <erlangparasu@gmail.com>
2019-10-11 14:57:33 +0800

    v2

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Created by: Erlang Parasu 2019.
+ * 
+ * Sample app to run additional task without blocking the http response
+ */
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -22,17 +28,21 @@ function runPendingTask() {
 
 // Middleware
 $pendingTaskMiddleware = function (Request $request, Response $response, $next) {
+    /** @var Response $response */
     $response = $next($request, $response);
     if (empty(getPendingTask())) {
         return $response;
     }
 
-    $content = $response->getBody()->getContents();
-    $size = $response->getBody()->getSize();
+    $content = (string) $response->getBody();
+    $size = mb_strlen($content);
 
+    if (ob_get_level() > 0) {
+        ob_end_flush();
+    }
     ignore_user_abort(true);
     header('Content-Type: application/json');
-    header('Connection: Close');
+    header('Connection: close');
     header('Keep-Alive: timeout=0, max=0');
     header('Content-Length: '.$size.'');
     echo $content;
@@ -46,21 +56,21 @@ $pendingTaskMiddleware = function (Request $request, Response $response, $next) 
 $app = new \Slim\App;
 $app->add($pendingTaskMiddleware);
 $app->get('/', function (Request $request, Response $response, array $args) {
-    setPendingTask(function () {
-        echo 'BG Start';
-
-        sleep(10); // eg. Resquest to an API
-        // exec('notepad'); // Test for Windows
-
-        echo 'BG End';
+    $strTime = date('Y_m_d__H_i_s');
+    setPendingTask(function () use ($strTime) {
+        // Sample of long running background task
+        echo 'pending task start';
+        sleep(10);
+        file_put_contents('log_for_request__'.$strTime.'.txt', '');
+        echo 'pending task end';
     });
 
-    $response->withJson([
+    $data = [
         'success' => true,
-        'time' => date('Y-m-d H:i:s'),
-    ]);
+        'time' => $strTime,
+    ];
 
-    return $response;
+    return $response->withJson($data);
 });
 
 $app->run();
